1d3feef6d4b91733841f8981f4d1632bce742a40
Flower1011 <95836694+Flower1011@users.noreply.github.com>
2022-06-02 22:45:09 -0400

    Update even.py

--- even.py
+++ even.py
@@ -1,3 +1,7 @@
+# Write a function called is_even with a parameter called file, which represents an open file containing a single number on each line. 
+Your is_even function should go through the open file line by line, determine if the number on that line is even or not and print: 
+'<number> <True/False>' depending on if the number is even or not. 
+
 #!/usr/bin/env python3
 
 ### IMPORT STATEMENTS ###
